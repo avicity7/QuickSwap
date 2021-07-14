7f9afe0ee763db39e1a3168635802b34f4f8566e
avicity7 <74177090+avicity7@users.noreply.github.com>
2021-07-14 09:27:12 +0800

    Update to FastSwap V2
    Liquidity querying added, buying refactored into a function. Testing needed on accuracy of queries.

--- FastSwap.py
+++ FastSwap.py
@@ -3,14 +3,34 @@ import time
 import pprint
 from web3.providers.eth_tester import EthereumTesterProvider
 from web3 import Web3
-personal_wallet, private, purchaseMethod = "","",int() 
+from web3.middleware import geth_poa_middleware # Needed for Binance
+from json import loads
+from decimal import Decimal
 
-while personal_wallet == "" and private == "": 
-    personal_wallet = input("Enter your personal address: ")
-    private = input("Enter your private key: ")
+personal_wallet = "" #Enter your personal address here
+private = "" #Enter your private key here
 
+def buyTokens(tokenA,tokenB,personal_wallet,private):
+    pancakeswap2_txn = contract.functions.swapExactETHForTokens(
+        1074184953676691292, # here setup the minimum destination token you want to have, you can do some math, or you can put a 0 if you don't want to care
+        [tokenA,tokenB],
+        personal_wallet,
+        (int(time.time()) + 1000000)
+        ).buildTransaction({
+        'from': personal_wallet,
+        'value': amount,#This is the Token(BNB) amount you want to Swap from
+        'gas': 250000,
+        'gasPrice': w3.toWei('10','gwei'),
+        'nonce': nonce,})
+    signed_txn = w3.eth.account.sign_transaction(pancakeswap2_txn, private_key=private)
+    tx_token = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    if tx_token != "": 
+        print("Success")
+        print("Txn hash: " + w3.toHex(tx_token))
+    else: 
+        print('Failed')
 
-while True:
+while True: 
     w3 = Web3(Web3.HTTPProvider('https://bsc-dataseed.binance.org/'))
     min_amount = 0 
     router = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
@@ -27,31 +47,64 @@ while True:
     path = [tokenA, tokenB]
     nonce = w3.eth.get_transaction_count(personal_wallet)
 
-    purchaseMethod = int(input("Enter 1 for regular buy and 2 for sniping: "))
-    if purchaseMethod == 1: 
-        pancakeswap2_txn = contract.functions.swapExactETHForTokens(
-        1074184953676691292, # here setup the minimum destination token you want to have, you can do some math, or you can put a 0 if you don't want to care
-        [tokenA,tokenB],
-        personal_wallet,
-        (int(time.time()) + 1000000)
-        ).buildTransaction({
-        'from': personal_wallet,
-        'value': amount,#This is the Token(BNB) amount you want to Swap from
-        'gas': 250000,
-        'gasPrice': w3.toWei('5','gwei'),
-        'nonce': nonce,
-        })
-
-        signed_txn = w3.eth.account.sign_transaction(pancakeswap2_txn, private_key=private)
-        tx_token = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-        if tx_token != "": 
-            print("Success")
-            print(w3.toHex(tx_token))
-        else: 
-            print('Failed')
-
-
     
 
+    purchaseMethod = int(input("Enter 1 for regular buy and 2 for sniping: "))
+    if purchaseMethod == 1: 
+        buyTokens(tokenA,tokenB,personal_wallet,private)
+
+    if purchaseMethod == 2: 
+        while True:
+            ETHER = 10 ** 18
+
+            WBNB = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
+            CAKE_ROUTER_V2 = w3.toChecksumAddress('0x10ed43c718714eb63d5aa57b78b54704e256024e')
 
 
+
+            ABI = loads('[{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"token0","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"name":"factory","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"_reserve0","type":"uint112"},{"internalType":"uint112","name":"_reserve1","type":"uint112"},{"internalType":"uint32","name":"_blockTimestampLast","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"}]')
+
+            def get_price(token, decimals, pair_contract, is_reversed, is_price_in_peg):
+                peg_reserve = 0
+                token_reserve = 0
+                (reserve0, reserve1, blockTimestampLast) = pair_contract.functions.getReserves().call()
+                
+                if is_reversed:
+                    peg_reserve = reserve0
+                    token_reserve = reserve1
+                else:
+                    peg_reserve = reserve1
+                    token_reserve = reserve0
+                
+                if token_reserve and peg_reserve:
+                    if is_price_in_peg:
+                        # CALCULATE PRICE BY TOKEN PER PEG
+                        price = (Decimal(token_reserve) / 10 ** decimals) / (Decimal(peg_reserve) / ETHER)
+                    else:
+                        # CALCULATE PRICE BY PEG PER TOKEN
+                        price = (Decimal(peg_reserve) / ETHER) / (Decimal(token_reserve) / 10 ** decimals)
+                    
+                    return price
+                    
+                return Decimal('0')
+
+
+
+            CAKE_FACTORY_V2 = w3.eth.contract(address=CAKE_ROUTER_V2, abi=ABI).functions.factory().call()
+
+            try:
+                token = w3.toChecksumAddress(tokenB)
+                pair = w3.eth.contract(address=CAKE_FACTORY_V2, abi=ABI).functions.getPair(token, WBNB).call()
+                pair_contract = w3.eth.contract(address=pair, abi=ABI)
+                is_reversed = pair_contract.functions.token0().call() == WBNB
+                decimals = w3.eth.contract(address=token, abi=ABI).functions.decimals().call()
+                is_price_in_peg = True
+                liquidityPresence = float(get_price(token, decimals, pair_contract, is_reversed, is_price_in_peg))
+
+                if liquidityPresence > 0:
+                    print("LIQUIDITY ADDED")
+                    break 
+
+            except:
+                print("Liqiuidity not added")
+        buyTokens(tokenA,tokenB,personal_wallet,private)
